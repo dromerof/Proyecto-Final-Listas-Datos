e4089fae9d2ba4e1e792873a887f4baa5cca88e8
Daniel Romero <dandres508@gmail.com>
2024-09-24 15:41:00 -0500

    Constructor de tarjeta de credito

--- src/TarjetaDeCredito.java
+++ src/TarjetaDeCredito.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class TarjetaDeCredito {
     private double limite;
     private double saldo;
     private List<Compra>listaDeCompra;
+
+    public TarjetaDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.listaDeCompra = new ArrayList<>();
+    }
+
+
+
 }
