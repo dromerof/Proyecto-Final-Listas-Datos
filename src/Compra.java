8d4c552379973a9bb867c2a8d6680f5a6fa7a8b6
Daniel Romero <dandres508@gmail.com>
2024-09-24 15:43:04 -0500

    contructor de compra

--- src/Compra.java
+++ src/Compra.java
@@ -1,2 +1,10 @@
 public class Compra {
+
+    private double valor;
+    private String descripcion;
+
+    public Compra(double valor, String descripcion) {
+        this.valor = valor;
+        this.descripcion = descripcion;
+    }
 }
